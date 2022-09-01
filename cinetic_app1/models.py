f594ed39bfe8afeca212df3e1310803d6df628ec
John Ovalle <john.ovalle4@gmail.com>
2022-08-31 22:10:35 -0500

    Versión completa de serializers.py y models.py

--- cinetic_app1/models.py
+++ cinetic_app1/models.py
@@ -37,18 +37,74 @@ class Empleado(models.Model):
     Contrasena = models.CharField(max_length=20)
     Ciudad = models.CharField(max_length=25)
     def __str__(self):
-        return self.CodigoCombo
+        return self.Identificacion
 
-class Venta(models.Model):
+class VentaProductos(models.Model):
     IdVenta = models.IntegerField(primary_key=True,unique=True)
     IdEmpleado = models.ForeignKey(Empleado, on_delete=models.PROTECT)
     FechaVenta = models.DateField(auto_now_add=True, null=False, blank=False)
+    def __str__(self):
+        return self.IdVenta
 
-class ListaCompra(models.Model):
+class ListaVentaProductos(models.Model):
     IdListaCompra = models.IntegerField(primary_key=True,unique=True)
     IdCombo = models.ForeignKey(Combo, on_delete=models.PROTECT)
     IdProducto = models.ForeignKey(Producto, on_delete=models.PROTECT)
-    IdVenta = models.ForeignKey(Venta, on_delete=models.PROTECT)
+    IdVenta = models.ForeignKey(VentaProductos, on_delete=models.PROTECT)
     Cantidad = models.IntegerField()
     Subtotal = models.IntegerField()
 
+class Pelicula(models.Model):
+    IdPelicula = models.IntegerField(primary_key=True,unique=True)
+    CodigoPelicula = models.CharField(max_length=10, unique=True)
+    NombrePelicula = models.CharField(max_length=30)
+    Genero = models.CharField(max_length=20)
+    Clasificacion = models.CharField(max_length=12)
+    NombreDirector = models.CharField(max_length=40)
+    FechaNacimiento = models.DateField(auto_now_add=False, null=False, blank=False)
+    Sinopsis = models.CharField(max_length=250)
+    Duracion = models.IntegerField()
+    def __str__(self):
+        return self.CodigoPelicula
+
+class Cinema(models.Model):
+    IdCinema = models.IntegerField(primary_key=True,unique=True)
+    CodigoCinema = models.CharField(max_length=5)
+    Ciudad = models.CharField(max_length=25)
+    Telefono = models.CharField(max_length=15)
+    Direccion = models.CharField(max_length=40)
+    Email = models.CharField(max_length=30)
+    def __str__(self):
+        return self.CodigoCinema
+class Sala(models.Model):
+    IdSala = models.IntegerField(primary_key=True,unique=True)
+    IdCinema = models.ForeignKey(Cinema, on_delete=models.PROTECT)
+    CodigoSala = models.CharField(max_length=5)
+    Nombre = models.CharField(max_length=20)
+    TipoSala = models.CharField(max_length=5)
+    Capacidad = models.IntegerField()
+    def __str__(self):
+        return self.CodigoSala
+
+class Funcion(models.Model):
+    IdFuncion = models.IntegerField(primary_key=True,unique=True)
+    IdPelicula = models.ForeignKey(Pelicula, on_delete=models.PROTECT)
+    IdSala = models.ForeignKey(Sala, on_delete=models.PROTECT)
+    CodigoFuncion = models.CharField(max_length=5)
+    Horario = models.CharField(max_length=7)
+    Fecha = models.DateField(auto_now_add=False, null=False, blank=False)
+    def __str__(self):
+        return self.CodigoFuncion
+
+class VentaBoletas(models.Model):
+    IdVentaBoletas = models.IntegerField(primary_key=True,unique=True)
+    IdEmpleado = models.ForeignKey(Empleado, on_delete=models.PROTECT)
+    FechaVenta = models.DateField(auto_now_add=False, null=False, blank=False)
+    def __str__(self):
+        return self.IdVentaBoletas
+class Boletas(models.Model):
+    IdBoleta = models.IntegerField(primary_key=True,unique=True)
+    IdFuncion = models.ForeignKey(Funcion, on_delete=models.PROTECT)
+    IdVentaBoletas = models.ForeignKey(VentaBoletas, on_delete=models.PROTECT)
+    NumeroSilla = models.IntegerField()
+    ValorBoleta = models.IntegerField()
--- cinetic_app1/serializers.py
+++ cinetic_app1/serializers.py
@@ -26,20 +26,62 @@ class Empleado_Serializer(serializers.ModelSerializer):
         model = Empleado
         fields = '__all__'
 
-class Venta_Serializer(serializers.ModelSerializer):
+class VentaProductos_Serializer(serializers.ModelSerializer):
     empleado = Empleado_Serializer(read_only=True)
     empleado_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Empleado.objects.all(), source='empleado')
     class Meta:
-        model = Venta
+        model = VentaProductos
         fields = '__all__'
 
-class ListaCompra_Serializer(serializers.ModelSerializer):
+class ListaVentaProductos_Serializer(serializers.ModelSerializer):
     producto = Producto_Serializer(read_only=True)
     producto_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Producto.objects.all(), source='producto')
     combo = Combo_Serializer(read_only=True)
     combo_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Combo.objects.all(), source='combo')
-    venta = Venta_Serializer(read_only=True)
-    venta_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Venta.objects.all(), source='venta')
+    venta = VentaProductos_Serializer(read_only=True)
+    venta_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=VentaProductos.objects.all(), source='venta')
     class Meta:
-        model = ListaCompra
+        model = ListaVentaProductos
+        fields = '__all__'
+
+class Pelicula_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pelicula
+        fields = '__all__'
+
+class Cinema_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cinema
+        fields = '__all__'
+
+class Sala_Serializer(serializers.ModelSerializer):
+    cinema = Cinema_Serializer(read_only=True)
+    cinema_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Cinema.objects.all(), source='cinema')
+    class Meta:
+        model = ListaVentaProductos
+        fields = '__all__'
+
+class Funcion_Serializer(serializers.ModelSerializer):
+    pelicula = Pelicula_Serializer(read_only=True)
+    pelicula_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Pelicula.objects.all(), source='pelicula')
+    sala = Sala_Serializer(read_only=True)
+    sala_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Sala.objects.all(), source='sala')
+    class Meta:
+        model = Funcion
+        fields = '__all__'
+
+class VentaBoletas_Serializer(serializers.ModelSerializer):
+    empleado = Empleado_Serializer(read_only=True)
+    empleado_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Empleado.objects.all(), source='emplado')
+    class Meta:
+        model = VentaBoletas
+        fields = '__all__'
+
+class Boletas_Serializer(serializers.ModelSerializer):
+    funcion = Funcion_Serializer(read_only=True)
+    funcion_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=Funcion.objects.all(), source='funcion')
+    ventaboletas = VentaBoletas_Serializer(read_only=True)
+    ventaboletas_id = serializers.PrimaryKeyRelatedField(write_only=True, queryset=VentaBoletas.objects.all(), source='ventaboletas')
+    class Meta:
+        model = Boletas
         fields = '__all__'
